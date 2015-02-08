40e913e561956d7a7d882f31847ece1d87b49598
Rick Song <ricksong@squareup.com>
2015-02-08 04:42:35 -0800

    DOESNT WORK

--- js-app/admin-frontend/app/controllers/device/edit.js
+++ js-app/admin-frontend/app/controllers/device/edit.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
+import EmberValidations from 'ember-validations';
+import BufferedMixin from 'ember-buffered-proxy/mixin';
 
-export default Ember.ObjectController.extend({
+export default Ember.ObjectController.extend(BufferedMixin, EmberValidations.Mixin, {
   needs: ['categories'],
   categories: Ember.computed.alias('controllers.categories.content'),
 
@@ -12,5 +14,24 @@ export default Ember.ObjectController.extend({
         value: category
       };
     });
-  }.property('categories.@each')
+  }.property('categories.@each'),
+
+  validations: {
+    category: {
+      presence: true
+    },
+    hardware: {
+      presence: true
+    },
+    operatingSystem: {
+      presence: true
+    },
+    storageCode: {
+      presence: true
+    },
+    barcode: {
+      presence: true
+    }
+  },
+  notValid: Ember.computed.not('isValid'),
 });
