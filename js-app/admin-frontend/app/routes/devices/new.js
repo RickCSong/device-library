0cd4d4e4eed126fe99d1a124c62cc83119ba4e75
Rick Song <ricksong@squareup.com>
2015-02-09 02:28:29 -0800

    Default category for device.new route

--- js-app/admin-frontend/app/routes/devices/new.js
+++ js-app/admin-frontend/app/routes/devices/new.js
@@ -5,7 +5,10 @@ import BufferedChangeRoute from 'admin-frontend/mixins/buffered-change-route';
 export default Ember.Route.extend(BufferedChangeRoute, {
   controllerName: 'device.edit',
   model: function() {
-    var device = this.store.createRecord('device');
+    var category = this.modelFor('category');
+    var device = this.store.createRecord('device', {
+      category: category
+    });
     return DeviceForm.create({
       content: device
     });
